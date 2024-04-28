991819778ac4e6b7cb7a5db337a7d80c8a9c22ae
Zhelizko Daniel <yokzz.programming@gmail.com>
2024-04-28 13:27:42 +0300

    Added 3 views(TaskList, TaskDetail, TaskCreate)

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    
+    STATUS_CHOICES = [
+        ["to_do", "To Do"],
+        ["in_progress", "In Progress"],
+        ["done", "Done"],
+    ]
+    
+    PRIORITY_CHOICES = [
+        ["low", "Low"],
+        ["medium", "Medium"],
+        ["high", "High"],
+        ["cricital", "Cricital"]
+    ]
+    
+    title = models.CharField(max_length=63)
+    description = models.TextField()
+    status = models.CharField(max_length=31, choices=STATUS_CHOICES, default="to_do")
+    priority = models.CharField(max_length=31, choices=PRIORITY_CHOICES, default="medium")
+    due_date = models.DateTimeField(null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from tasks.models import Task
+from tasks.forms import TaskForm
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "tasks/task_list.html"
+    
+    
+class TaskDetailView(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = "tasks/task_detail.html"
+    
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "tasks/task_form.html"
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
+    
